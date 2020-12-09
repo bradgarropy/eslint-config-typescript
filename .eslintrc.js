eeff82ed530f500b05c233852feddfe39b3ee664
Brad Garropy <bradgarropy@gmail.com>
2020-12-09 14:15:51 -0600

    typescript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,49 +5,19 @@ module.exports = {
         jest: true,
         node: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react/recommended",
-        "./rules/eslint-best-practices.js",
-        "./rules/eslint-errors.js",
-        "./rules/eslint-es6.js",
-        "./rules/eslint-node.js",
-        "./rules/eslint-style.js",
-        "./rules/eslint-variables.js",
-        "./rules/react.js",
-        "./rules/react-hooks.js",
-        "./rules/svelte.js",
-        "prettier",
-    ],
+    extends: ["@bradgarropy/eslint-config"],
     overrides: [
         {
             files: ["*.ts", "*.tsx"],
             extends: ["plugin:@typescript-eslint/recommended"],
         },
-        {
-            files: ["*.svelte"],
-            processor: "svelte3/svelte3",
-        },
     ],
     parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
+        ecmaFeatures: {},
         sourceType: "module",
     },
-    plugins: [
-        "jsx-a11y",
-        "react",
-        "react-hooks",
-        "svelte3",
-        "@typescript-eslint",
-    ],
+    plugins: ["@typescript-eslint"],
     parser: "@typescript-eslint/parser",
     rules: {},
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
+    settings: {},
 }
