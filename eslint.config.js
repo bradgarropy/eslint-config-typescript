dfb03b527251be3a0f2c6b9a755e2de94d2e4655
Brad Garropy <bradgarropy@gmail.com>
2024-08-15 14:50:53 -0500

    done.

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+const config = [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.jest,
+                ...globals.es2021,
+            },
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaFeatures: {},
+                sourceType: "module",
+                project: "./tsconfig.json",
+            },
+        },
+    },
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            "@typescript-eslint": tseslint.plugin,
+        },
+    },
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/consistent-type-imports": [
+                "error",
+                {
+                    prefer: "type-imports",
+                    disallowTypeAnnotations: true,
+                    fixStyle: "separate-type-imports",
+                },
+            ],
+            "@typescript-eslint/consistent-type-exports": [
+                "error",
+                {fixMixedExportsWithInlineTypeSpecifier: false},
+            ],
+        },
+    },
+]
+
+export default config
